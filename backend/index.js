7d6280d3449dcd4f7af4be5164910075b49919cb
vishnu-r-2023 <vishnu.r2023csbs@sece.ac.in>
2026-03-17 15:49:42 +0530

    latest updates

--- backend/index.js
+++ backend/index.js
@@ -8,13 +8,11 @@ const PORT = process.env.PORT || 5000;
 async function start() {
   await connectDB();
   app.listen(PORT, () => {
-    // eslint-disable-next-line no-console
     console.log(`API listening on port ${PORT}`);
   });
 }
 
 start().catch((err) => {
-  // eslint-disable-next-line no-console
   console.error("Failed to start server:", err);
   process.exit(1);
 });
